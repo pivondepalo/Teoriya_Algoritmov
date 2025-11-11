554da39230cc9aba75998aea9a5812983d619c43
pivondepalo <9307857@stud.op.edu.ua>
2025-11-11 16:56:32 +0200

    Implement insertion sort and array printing methods

--- laba1/Insertion Sort.java
+++ laba1/Insertion Sort.java
@@ -1,1 +1,19 @@
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int currentValue = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > currentValue) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = currentValue;
+        }
+    }
 
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
