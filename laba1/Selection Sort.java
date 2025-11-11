d5f4f6b85b39d24e4dfaeebae4b75545b6c197b2
pivondepalo <9307857@stud.op.edu.ua>
2025-11-11 16:54:13 +0200

    Create Selection Sort.java

---
+++ laba1/Selection Sort.java
@@ -0,0 +1,50 @@
+public class SortLabVar8 {
+    public static void main(String[] args) {
+        int[] arr1 = {69, 52, 97, 27, 10, 88, 29, 1, 24};
+        int[] arr2 = arr1.clone();
+
+        System.out.println("\n=== Вхідна послідовність (варіант 8) ===");
+        printArray(arr1);
+
+        selectionSort(arr1);
+        System.out.println("\n=== Сортування вибором (Selection Sort) ===");
+        printArray(arr1);
+
+        insertionSort(arr2);
+        System.out.println("\n=== Сортування вставками (Insertion Sort) ===");
+        printArray(arr2);
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int currentValue = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > currentValue) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = currentValue;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
